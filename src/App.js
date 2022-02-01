2eadcd694b4c3b6bfb2834a9e28c1a5c7eab258f
MOONLAYT500 <97441013+MOONLAYT500@users.noreply.github.com>
2022-02-01 11:55:31 +0300

    my heroku api implemented

--- src/App.js
+++ src/App.js
@@ -7,25 +7,26 @@ import Tasks from './components/Tasks/Tasks';
 import { Pagination, message } from 'antd';
 
 function App() {
-  const [filteredTodos, setFilteredTodos] = useState([]);//all tasks
-  const [currentPage, setCurrentPage] = useState(1);//current page from pagination
+  const [filteredTodos, setFilteredTodos] = useState([]); //all tasks
+  const [currentPage, setCurrentPage] = useState(1); //current page from pagination
   const [todosStatus, setTodosStatus] = useState('all'); // filter by done from sorter
   const [createdAt, setCreatedAt] = useState('desc'); // filter vu date from sorter
-  const [todosCount, setTodosCount] = useState(0);//todos count from server
-  const [postsPerPage, setPostsPerPage] = useState(5);// post per page 1
+  const [todosCount, setTodosCount] = useState(0); //todos count from server
+  const [postsPerPage, setPostsPerPage] = useState(5); // post per page 1
   const api = axios.create({
-    baseURL: 'https://todo-api-learning.herokuapp.com/v1/',// creating baseURL
+    baseURL: 'https://crud-api12.herokuapp.com/api/', // creating baseURL
   });
 
   useEffect(() => {
-    getTodos();// recieving todos from api and rendering page
+    getTodos(); // recieving todos from api and rendering page
   }, [todosStatus, createdAt, currentPage]); // triggers to render
 
-  api.interceptors.response.use( // error han
+  api.interceptors.response.use(
+    // error han
     (response) => response,
     (error) => {
       let errorMessage;
-      let res = error.request.response;
+      const res = error.request.response;
       if (!res) {
         errorMessage = 'No responce';
       }
@@ -39,44 +40,53 @@ function App() {
     }
   );
 
-  const getTodos = async () => { // getting todos from server
-    const res = await api.get(`tasks/3`, { //sending request 
-      params: { // form new request
+  const getTodos = async () => {
+    // getting todos from server
+    const res = await api.get(`todos`, {
+      //sending request
+      params: {
+        // form new request
         filterBy: todosStatus === 'all' ? '' : todosStatus, // done status
         order: createdAt, // time status
         pp: postsPerPage, // todos on page count
         page: currentPage, // current page to return
       },
     });
+    console.log(res);
     // if(res.data.tasks.length === 0) setTodosStatus('all')
-    if (res.data.tasks.length === 0 && currentPage > 1){ // return on previous page? if current is empty 
-    setCurrentPage(currentPage - 1)}
-    
-    setTodosCount(res.data.count);//seting total todos count
-    setFilteredTodos(res.data.tasks); // sitting final todos to render
+    if (res.data.todos.length === 0 && currentPage > 1) {
+      // return on previous page? if current is empty
+      setCurrentPage(currentPage - 1);
+    }
+
+    setTodosCount(res.data.count); //seting total todos count
+    setFilteredTodos(res.data.todos); // sitting final todos to render
   };
 
-  const createTodo = async (input) => { // create and post new todo to server
-    const todo = { //new todo
+  const createTodo = async (input) => {
+    // create and post new todo to server
+    const todo = {
+      //new todo
       name: input, // todo text
       done: false, // done status
     };
-    if (!todo.name || /^\s*$/.test(todo.name)) { // empty string filter
+    if (!todo.name || /^\s*$/.test(todo.name)) {
+      // empty string filter
       message.error('Empty string not allowed'); // message to client
       return;
     }
-    await api.post(`task/3`, todo); // posting new todo to api
+    await api.post(`todos`, todo); // posting new todo to api
     await getTodos(); // getting all todos to render
   };
 
   const editTodo = async (todo, id) => {
-    let res = await api.patch(`task/3/${id}`, todo) // sending edited todo to api
+    let res = await api.patch(`todos/${id}`, todo); // sending edited todo to api
     await getTodos(); //rendering edited
     return res;
   };
 
   const deleteTodo = async (id) => {
-    await api.delete(`task/3/${id}`);// deleting todo by id
+    await api.delete(`/todos/${id}`); // deleting todo by id
     await getTodos(); // rendering todos after deleting
   };
 
@@ -94,15 +104,12 @@ function App() {
       <h1 className="header">To-Do List</h1>
       <div className="container">
         <Creator createTodo={createTodo} />
-        <Sorter
-          statusFilter={statusFilter}
-          createdAtFilter={createdAtFilter}
-        />
+        <Sorter statusFilter={statusFilter} createdAtFilter={createdAtFilter} />
         {todosCount === 0 ? (
           <div>No Tasks</div> // if filtered tasks is empty
         ) : (
           <Tasks
-            editTodo={editTodo} 
+            editTodo={editTodo}
             deleteTodo={deleteTodo}
             filteredTodos={filteredTodos}
           />
